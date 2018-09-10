7bc13b22376eeb3fab488b0fa87908807df67352
muicv1991@gmail.com <Anhyeuem112>
2018-09-10 16:00:46 +0700

    hh

--- app/src/main/java/com/mghstudio/ringtonemaker/utils/AppConstants.java
+++ app/src/main/java/com/mghstudio/ringtonemaker/utils/AppConstants.java
@@ -7,6 +7,9 @@ package com.mghstudio.ringtonemaker.utils;
 public class AppConstants {
     public final static String URL_CLIENT_CONFIG = "https://minigameshouse.us:8443/adsserver-v1/client_config";
     public final static String URL_CREATE_SHORTCUT = "https://minigameshouse.us:8443/adsserver-v1/create_shortcut";
-//    public final static String URL_ADS_CONFIG = "https://minigameshouse.us:8443/adsserver-v1/checkads";
-//    public final static String URL_CONFIG = "https://config-app-game-4.firebaseio.com/com_mghstudio_ringtonemaker.json";
+
+    public static final int ALARM_SCHEDULE_MINUTES = 2;
+    public static final String ID_FULL_SERVICE = "/21617015150/734252/21734809637";
+    public static final String ID_FULL_FB_SERVICE = "2199797023369826_2351005924915601";
+
 }
